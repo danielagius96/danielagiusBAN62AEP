0a2238d08e436b15cb4ae09db0bf52c18001acde
Daniel Agius <Daniel.Agius.c10239@mcast.edu.mt>
2021-01-20 19:41:26 +0100

    Added Product buttons in the Navbar, and added validation to product/create inputs

--- Solution1/ShoppingCart.Application/ViewModels/ProductViewModel.cs
+++ Solution1/ShoppingCart.Application/ViewModels/ProductViewModel.cs
@@ -7,21 +7,25 @@ namespace ShoppingCart.Application.ViewModels
 {
     public class ProductViewModel
     {
+        //Here we are including all reuired so that fields will have full validation
      
         public Guid Id { get; set; }
 
         [Required(ErrorMessage ="Please input name of product")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please input a Description")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Please input the price of the product")]
         [Range(1, 100)]
         [DataType(DataType.Currency)]
         public double Price { get; set; }
 
-
+        [Required(ErrorMessage = "Please input a category")]
         public CategoryViewModel Category { get; set; }
 
+        [Required(ErrorMessage = "Please input an image")]
         public string ImageUrl { get; set; }
 
         public int Stock { get; set; }
